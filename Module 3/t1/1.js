3fb292fecda874db47de1b63d5d82cfc32bb668a
veeveevu <vuhavy0805@gmail.com>
2024-11-30 23:29:23 +0200

    Javascript Module 3 Exercise

---
+++ Module 3/t1/1.js
@@ -0,0 +1,8 @@
+document.getElementById('target').innerHTML = `
+    <li>First item</li>
+    <li>Second item</li>
+    <li>Third item</li>
+`;
+
+
+document.getElementById('target').classList.add('my-list');
